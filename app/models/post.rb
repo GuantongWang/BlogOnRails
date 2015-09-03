4a83f1e606db725fa7e908afc55190f8ff179e0b
wangguantong <wangguantong@wangguantongdeMacBook-Pro>
2015-09-04 01:00:21 +0800

    add kindeditor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  def get_feature_pic
+    @match = content.match(/<img.*?>/)
+    if @match
+      @match[0]
+    else
+      '<img src="/uploads/default.jpeg" alt="" />'
+    end
+  end
+
+  def get_preview
+    content.gsub(/<img.*?>/,"")
+  end
 end
